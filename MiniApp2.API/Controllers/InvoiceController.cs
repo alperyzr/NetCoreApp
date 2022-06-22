167977de17477899a40c7bef615f97b8ab745534
alper yazır <alper.yazir@outlook.com>
2022-06-23 00:14:42 +0300

    SharedLibrary için extension methodu yazıldı. MınıAPI lerin statup.cs, appsettingsleri ve controller kısımları düzenlendi

---
+++ MiniApp2.API/Controllers/InvoiceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+
+namespace MiniApp2.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        public IActionResult GetInvoices()
+        {
+            //Veri tabanında userName e göre datayı çek
+            var userName = HttpContext.User.Identity.Name;
+
+            //Veri tabanında Id ye göre datayı çek
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            return Ok($"Fatura İşlemleri => UserName:{userName} userId:{userId}");
+
+        }
+    }
+}
---
+++ SharedLibrary/Extensions/CustomTokenAuth.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using SharedLibrary.Configuration;
+using SharedLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Extensions
+{
+    //extension classlar static yazılmalıdır.
+    public static class CustomTokenAuth
+    {
+        public static void AddCustomTokenAuth(this IServiceCollection services, CustomTokenOption tokenOptions)
+        {
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                //Elimizdeki iki şemayı burda birbiri ile konuşturuyoruz.
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+            {
+                //servise.Add diyerek eklemiş bir nesneden instance almka için kullanılır.                
+                opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    //NetCoreApp' taki appsettings.json dosyasındaki alanlar ilgili alanlara setlenir
+                    ValidIssuer = tokenOptions.Issuer,
+
+                    //Token'daki Audince dizi olduğu için sıfırıncı indis'ten instance aldık
+                    ValidAudience = tokenOptions.Audience[0],
+                    IssuerSigningKey = SingService.GetSymmetricSecuriyKey(tokenOptions.SecurityKey),
+
+                    //Issuer İmzası Doğrulanır
+                    ValidateIssuerSigningKey = true,//imzayı doğrular
+                    ValidateAudience = true,//Audice doğrular
+                    ValidateIssuer = true,//Issuer doğrular
+                    ValidateLifetime = true,//Token Ömrünü kontrol eder
+
+                    //Token ömrüne otomatik olarak verilen sürenin 5dk fazlasını verir. Zero komutu o 5dk lık default süreyi kaldırır.
+                    //İsteğe bağlı olarak eklenir.
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+        }
+    }
+}
